8c1572e8c3f9065b22fc56b0d38558ab4e2da14b
Jacobo J. Pedrosa <jacoboj.pedrosa@Jacobos-Laptop.local>
2025-07-18 12:25:17 +0200

    streamlit frontend definition

--- main.py
+++ main.py
@@ -3,7 +3,27 @@ import streamlit as st
 
 
 def main():
-    pass
+    st.title("Document Search App")
+
+
+    uploaded_files = st.file_uploader("Upload documents", type=["txt", "pdf"], accept_multiple_files=True)
+    if uploaded_files:
+        documents = []
+        for file in uploaded_files:
+            print(f"{file.name} - {file.type}")
+        #     if file.type == "text/plain":
+        #         documents.append(file.getvalue().decode("utf-8"))
+
+    query = st.text_input("Search query:")
+    if query:
+        print(f"query: {query}")
+    
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
